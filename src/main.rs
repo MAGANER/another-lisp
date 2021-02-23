fe136d70a36b3b98b7d78a01156ac9dda2bf8218
MAGANER <maganer.krik@mail.ru>
2021-02-23 19:27:12 +0300

    wrote s-expr parser

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,21 @@ fn main() {
     }
     let script = fs::read_to_string(&args[1]).expect("can not open file!");
     let tokens = parser::tokenize(script);
+    let proto_tree = parser::compute_token_depth(&tokens);
+    for node in proto_tree.iter()
+    {
+        println!("{}{}",mult_str(' ', node.depth),node.token);
+    }
+}
 
-    println!("{:?}",tokens);
+
+fn mult_str(string:char,rep_number:i32) -> String
+{
+    let mut new_str:String = String::new();
+    for id in 0..rep_number+1
+    {
+        new_str.push(string);
+    }
+
+    new_str
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
 pub fn tokenize(file:String) -> Vec<String> {
+    //return vector with tokens
     let mut tokens:Vec<String> = Vec::new();
     
     let mut token:String = String::from("");
@@ -8,7 +9,7 @@ pub fn tokenize(file:String) -> Vec<String> {
         {
              if ch == '(' || ch == ')' 
             { 
-                 if token.len() > 0
+                if token.len() > 0
                 {
                        tokens.push(token.clone());
                        token.clear();
@@ -18,12 +19,52 @@ pub fn tokenize(file:String) -> Vec<String> {
             else if ch != ' ' { token.push(ch); }
             else
             {
-                 tokens.push(token.clone());
-                 token.clear();
+                tokens.push(token.clone()); 
+                token.clear();
             }
         }
     
     };
     
-    tokens
+    delete_empty_tokens(tokens)
+}
+fn delete_empty_tokens(tokens:Vec<String>) -> Vec<String>
+{
+    let mut cleared:Vec<String> = Vec::new();
+    for token in tokens.iter()
+    {
+        if token != ""
+        {
+            cleared.push(token.clone());
+        }
+    }
+    cleared
+}
+
+#[derive(Debug)]
+pub struct Node
+{
+    pub token:String,
+    pub depth:i32
+}
+pub fn compute_token_depth(tokens:&Vec<String>) ->Vec<Node>
+{
+    let mut proto_tree:Vec<Node> = Vec::new();
+    
+    let mut depth = 0;
+    for token in tokens.iter()
+    {
+        if token == "(" { depth += 1; }
+        if token == ")" { depth -= 1; }
+
+        let not_bracket = token != "(" && token != ")";
+
+        if not_bracket
+        {
+            let curr_node = Node{token:token.clone(),depth:depth};
+            proto_tree.push(curr_node);
+        }
+    }
+
+    proto_tree
 }
