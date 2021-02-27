2f122949cd250381074dee76f7458bceb51a08eb
MAGANER <maganer.krik@mail.ru>
2021-02-27 14:07:56 +0300

    added function to check type of value

---
+++ src/interpreter/type_checker.rs
@@ -0,0 +1,23 @@
+pub enum Type
+{
+    Int,
+    Float,
+    Bool,
+    String
+}
+
+
+fn get_value_type(value:&String) -> Type
+{
+    let mut numeric_counter = 0;
+    let mut has_point = false;
+    for ch in value.chars()
+    {
+        if ch.is_numeric() { numeric_counter+=1; }
+        if ch == '.'       { has_point = true  ; }   
+    }
+    if numeric_counter == value.len() { Type::Int }
+    else if has_point && numeric_counter == value.len() - 1 { Type::Float }
+    else if value == "True" || value == "False" { Type::Bool }
+    else { Type::String }
+}
