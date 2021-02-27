56b0855870338e938c5f25e9fdebec6b2775169b
MAGANER <maganer.krik@mail.ru>
2021-02-27 20:15:58 +0300

    there are 2 options to see AST and to printl all computation results

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::process;
 
 mod interpreter;
 use interpreter::parser;
-
+use interpreter::reducer;
 
 fn main() {
     let args:Vec<String> = env::args().collect();
@@ -14,14 +14,36 @@ fn main() {
     }
     let script = fs::read_to_string(&args[1]).expect("can not open file!");
     let tokens = parser::tokenize(script);
-    let dtree  = parser::parse(&tokens);
-    for node in dtree.nodes.iter()
+    let trees  = parser::parse(&tokens);
+
+    if args.contains(&String::from("tree"))
     {
-        println!("{}{}",mult_str(' ', node.depth),node.token);
+        print_tree(&trees);
     }
+    if args.contains(&String::from("print"))
+    {
+        println!("result:");
+        for tree in trees.iter()
+        {
+            let result = reducer::compute_whole_tree(tree);
+            println!("{}",result);
+        }
+    }
+
+ 
 }
 
-
+fn print_tree(trees:&Vec<parser::DTree>)
+{
+    for tree in trees.iter()
+    {
+        for node in tree.nodes.iter()
+        {
+            println!("{}{}",mult_str(' ', node.depth),node.token);
+        }
+        println!("------");
+    }
+}
 fn mult_str(string:char,rep_number:i32) -> String
 {
     let mut new_str:String = String::new();
