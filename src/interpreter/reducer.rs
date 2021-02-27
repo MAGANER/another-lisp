69136a9a52b1920856189e343602a9d326dfe1cb
MAGANER <maganer.krik@mail.ru>
2021-02-27 20:07:12 +0300

    made function to compute whole tree

--- src/interpreter/reducer.rs
+++ src/interpreter/reducer.rs
@@ -1,5 +1,7 @@
 use super::parser;
 use super::type_checker;
+use std::process;
+
 fn get_node_of_depth(depth:i32,dtree:&parser::DTree) -> parser::DTree
 {
     //return sub tree with passed depth
@@ -32,28 +34,176 @@ fn get_tree_max_depth(dtree:&parser::DTree) -> i32
                 -1
            }
 }
-fn get_arguments(subtree:&parser::DTree)    -> Vec<String>
+
+
+fn get_arguments(subtree:&parser::DTree) -> Vec<String>
 {
     let mut args:Vec<String> = Vec::new();
-    for id in 1..subtree.nodes.len()-1
+    for id in 1..subtree.nodes.len()
     {
         args.push(subtree.nodes[id].token.clone());
     }
 
     args
 }
-fn compute_sub_tree(subtree:&parser::DTree) //-> Option<String>
+fn check_arguments_types(args:&Vec<String>)
+{
+    let _type = type_checker::get_value_type(&args[0]);
+    for arg in args.iter()
+    {
+        let curr_type = type_checker::get_value_type(&arg);
+        if curr_type != _type
+        {
+            println!("incorrect arguments:{:?}",args);
+            println!("they should have the same types!");
+            process::exit(-1);
+        }
+    }
+}
+
+fn compute_sub_tree(subtree:&parser::DTree) -> String
 {
     let operation = subtree.nodes[0].token.clone();
     let args      = get_arguments(subtree);
+    check_arguments_types(&args);
+
+
+    let result = match operation.as_str()
+    {
+        "+" => compute_binary_math_operation(Operator::Plus,   &args),
+        "-" => compute_binary_math_operation(Operator::Minus,  &args),
+        "*" => compute_binary_math_operation(Operator::Mult,   &args),
+        "/" => compute_binary_math_operation(Operator::Divide, &args),
+        "&" => compute_binary_math_operation(Operator::And,    &args),
+        "|" => compute_binary_math_operation(Operator::Or,     &args),
+        _ => {
+            println!("no such operation:{}",operation);
+            process::exit(-1);
+         }
+    };
+
+    result
 }
 
-pub fn compute_whole_tree(dtree:&parser::DTree)
+enum Operator
+{
+    Plus,
+    Minus,
+    Divide,
+    Mult,
+    And,
+    Or
+}
+fn compute_binary_math_operation(op:Operator,args:&Vec<String>) -> String
+{
+    let mut result:String = String::new();
+    let _type = type_checker::get_value_type(&args[0]);
+    for arg in args.iter()
+    {
+        if _type == type_checker::Type::Int
+        {
+            let val = arg.parse::<i32>().unwrap();
+            if result.is_empty()
+            {
+                result.push_str(&val.to_string());
+            } else {
+                        let prev_val = result.parse::<i32>().unwrap();
+                        let new_val  = match op 
+                                       {
+                                            Operator::Plus => prev_val + arg.parse::<i32>().unwrap(),
+                                            Operator::Minus=> prev_val - arg.parse::<i32>().unwrap(),
+                                            Operator::Mult => prev_val * arg.parse::<i32>().unwrap(),
+                                            Operator::Divide=>prev_val / arg.parse::<i32>().unwrap(),
+                                            _ => {
+                                                    println!("can not apply not arithmetic operator to Int!");
+                                                    process::exit(-1);
+                                                 }
+                                       };
+                        result = new_val.to_string();
+                   }
+        }
+        if _type == type_checker::Type::Float
+        {
+            let val = arg.parse::<f32>().unwrap();
+            if result.is_empty()
+            {
+                result.push_str(&val.to_string());
+            } else {
+                        let prev_val = result.parse::<f32>().unwrap();
+                        let new_val  = match op 
+                                       {
+                                            Operator::Plus => prev_val + arg.parse::<f32>().unwrap(),
+                                            Operator::Minus=> prev_val - arg.parse::<f32>().unwrap(),
+                                            Operator::Mult => prev_val * arg.parse::<f32>().unwrap(),
+                                            Operator::Divide=>prev_val / arg.parse::<f32>().unwrap(),
+                                            _ => {
+                                                    println!("can not apply not arithmetic operator to Float value!");
+                                                    process::exit(-1);
+                                                 }
+                                       };
+                        result = new_val.to_string();
+                   }
+        }
+        if _type == type_checker::Type::String
+        {
+            if result.is_empty()
+            {
+                result.push_str(&arg);
+            } else {
+                let new_val = match op
+                                {
+                                    Operator::Plus => result + arg,
+                                    _ => {
+                                            println!("can not apply not + to value of String type!");
+                                            process::exit(-1);
+                                         }
+                                };
+                result = new_val;
+                    }
+        }
+        if _type == type_checker::Type::Bool
+        {
+            if result.is_empty()
+            {
+                result.push_str(&arg);
+            } else {
+                let new_val = match op
+                                {
+                                    Operator::And => {
+                                                        if result == "True" && arg == "True" {"True"}
+                                                        else {"False"}
+                                                     },
+                                    Operator::Or  => {
+                                                        if result == "True" || arg == "True" {"True"}
+                                                        else {"False"}
+                                                     },
+                                    _ => {
+                                            println!("Bool doesn't support such operators!");
+                                            process::exit(-1);
+                                         }
+                                };
+                                result = String::from(new_val);
+                   }
+        }
+    }
+
+    result
+}
+
+pub fn compute_whole_tree(dtree:&parser::DTree) -> String
 {
     let mut begin = get_tree_max_depth(dtree);
+    let mut result:String = String::new();
     while begin > 0
     {
-        let subtree = get_node_of_depth(begin, dtree);
+        let mut subtree = get_node_of_depth(begin, dtree);
+        if !result.clone().is_empty()
+        {
+            subtree.nodes.push(parser::Node{token:result.clone(),depth:begin});
+        }
+        result = compute_sub_tree(&subtree);
         begin -= 1;
     }
+    result
 }
+
