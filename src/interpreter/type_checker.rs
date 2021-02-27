52dd5919913822a87ec8e6f729dc8e24074ef1db
MAGANER <maganer.krik@mail.ru>
2021-02-27 20:40:19 +0300

    fixed to prevent add single - character as negative number

--- src/interpreter/type_checker.rs
+++ src/interpreter/type_checker.rs
@@ -12,14 +12,20 @@ pub fn get_value_type(value:&String) -> Type
 {
     let mut numeric_counter = 0;
     let mut has_point = false;
+    let mut is_negative = false;
     for ch in value.chars()
     {
-        if ch == '-'       { numeric_counter+=1; }
+        if ch == '-'       { is_negative = true; }
         if ch.is_numeric() { numeric_counter+=1; }
         if ch == '.'       { has_point = true  ; }   
     }
-    if numeric_counter == value.len() { Type::Int }
-    else if has_point && numeric_counter == value.len() - 1 { Type::Float }
-    else if value == "True" || value == "False" { Type::Bool }
+
+    let is_int   = numeric_counter == value.len() ;
+    let is_float = has_point && numeric_counter == value.len() - 1;
+    let is_bool  = value == "True" || value == "False";
+
+    if      is_int   || is_negative ^ (!is_int   || is_negative)   { Type::Int   }
+    else if is_float || is_negative ^ (!is_float || is_negative) { Type::Float }
+    else if is_bool  { Type::Bool  }
     else { Type::String }
 }
