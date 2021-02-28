c21fdc93315711ff82f135ecdb27eedf8a749077
MAGANER <maganer.krik@mail.ru>
2021-02-28 23:19:28 +0300

    added > function

--- src/interpreter/env.rs
+++ src/interpreter/env.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::process;
 use super::expression;
 
 #[derive(Clone)]
@@ -51,6 +52,53 @@ pub fn default_env() -> Env {
           let sum_of_rest = floats[1..].iter().fold(first, |sum, a| sum / a);
           
           Ok(expression::Expr::Number(sum_of_rest))
+        }
+      )
+    );
+    data.insert(
+      ">".to_string(),
+      expression::Expr::Func(
+        |args: &[expression::Expr]| -> Result<expression::Expr, expression::Err> {
+          let floats = parse_list_of_floats(args);
+
+          let result = 
+          match floats
+          {
+            Ok(val) => {
+                            if val.len() < 2
+                            {
+                              println!("not enough arguments for >");
+                              process::exit(-1);
+                            }
+                            let mut results:Vec<bool> = Vec::new();
+                            let mut counter = 0;
+                            while counter < val.len()
+                            {
+                              let curr = val[counter];
+                              if counter+1 < val.len()
+                              {
+                                let next = val[counter+1];
+                                let result = curr > next;
+                                results.push(result);
+                              }
+                              counter += 1;
+                            }
+
+                            results.iter().fold(results[0],|a,b| a && *b)
+                       },
+            Err(err_val) => {
+                                match err_val
+                                {
+                                  expression::Err::Reason(v) => { println!("{}",v);
+                                                                  process::exit(-1); 
+                                                                }
+                                }
+                            },
+
+                    
+          };
+
+          Ok(expression::Expr::Bool(result))
         }
       )
     );    
