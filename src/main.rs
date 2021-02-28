d34827abe5d91ef422b182eec94cfe8b6594814f
MAGANER <maganer.krik@mail.ru>
2021-02-28 12:25:15 +0300

    made output for result of script

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::process;
 
 mod interpreter;
 use interpreter::parser;
+use interpreter::eval;
+use interpreter::expression;
 
 fn main() {
     let args:Vec<String> = env::args().collect();
@@ -11,6 +13,22 @@ fn main() {
         println!("not enough arguments!");
         process::exit(0);
     }
+
+    let mut env = eval::env::default_env();
+
     let script = fs::read_to_string(&args[1]).expect("can not open file!");
+    let tokens = parser::tokenize(script);
+    let tree   = parser::parse(&tokens);
+    let result = eval::eval(&tree.unwrap().0,&mut env);
+
+
+    let result = result.unwrap();
+    match result
+    {
+      expression::Expr::Symbol(val) => println!("{}",val),
+      expression::Expr::Number(val) => println!("{}",val),
+      expression::Expr::List(_)   => (),
+      expression::Expr::Func(_)   => (),
+    };
  
 }
