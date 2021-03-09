931c3c5c786a737ae74876cd4b69951fd172aa71
MAGANER <maganer.krik@mail.ru>
2021-03-09 17:43:10 +0300

    added lambdas. you can define it and pass arguments to compute

--- src/interpreter/eval.rs
+++ src/interpreter/eval.rs
@@ -1,31 +1,31 @@
 use super::expression;
 pub use super::env;
+use std::collections::HashMap;
 
-pub fn eval(exp: &expression::Expr, env: &mut env::Env) -> Result<expression::Expr, expression::Err> {
-    match exp {
-        expression::Expr::Symbol(k) =>
-          
-          if expression::is_string_value(k)
-          {
-              Ok(exp.clone())
-          } else 
-          {
-            env.data.get(k)
-            .ok_or(
-              expression::Err::Reason(
-                format!("unexpected symbol k={}", k)
-              )
-            )
-            .map(|x| x.clone())
-          }
-      ,
-      expression::Expr::Number(_a) => Ok(exp.clone()),
-      expression::Expr::Bool(_a)   => Ok(exp.clone()),
+pub fn eval(exp: &expression::Expr, env: &mut env::Env) -> Result<expression::Expr, expression::Err> 
+{
+    match exp 
+    {
+      expression::Expr::Number(_) => Ok(exp.clone()),
+      expression::Expr::Bool(_)   => Ok(exp.clone()),
+      expression::Expr::Symbol(k) =>
+      {
+         if expression::is_string_value(k)
+        {
+             Ok(exp.clone())
+        } else 
+              {    
+                 env.data.get(k)
+                         .ok_or(expression::Err::Reason(format!("unexpected symbol k={}", k)))
+                         .map(|x| x.clone())
+              }
+        },
       expression::Expr::List(list) => 
       {
         let first_form = list
-          .first()
-          .ok_or(expression::Err::Reason("expected a non-empty list".to_string()))?;
+                          .first()
+                          .ok_or(expression::Err::Reason("expected a non-empty list".to_string()))?;
+
         let arg_forms = &list[1..];
         match eval_built_in_form(first_form, arg_forms, env)
         {
@@ -65,6 +65,7 @@ fn eval_built_in_form
       match s.as_ref() {
         "if"    => Some(compute_if(arg_forms , env)),
         "def"   => Some(compute_def(arg_forms, env)),
+        "fn"    => Some(process_lambda(arg_forms, env)),
         "print" => {print_data(arg_forms, env); Some(Ok(expression::Expr::Bool(true)))},
         _       => None,
       },
@@ -105,7 +106,7 @@ fn compute_def(arg_forms: &[expression::Expr], env: &mut env::Env) -> Result<exp
   //init variable into the environment
 
   //it takes only variable name and it's value(atom or expression to compute)
-  if arg_forms.len() > 2 
+  if arg_forms.len() != 2 
   {
     return Err(expression::Err::Reason("def can only have two arguments ".to_string()))
   } 
@@ -133,6 +134,125 @@ fn compute_def(arg_forms: &[expression::Expr], env: &mut env::Env) -> Result<exp
   
   Ok(first_form.clone())
 }
+fn define_lambda(arg_forms:&[expression::Expr]) -> Result<expression::Expr, expression::Err>
+{
+  let args = &arg_forms[0];
+  let _args = match args
+  {
+    expression::Expr::List(_) => args,
+    _ => 
+    {
+      println!("arguments must be list!");
+      std::process::exit(-1);
+    }
+  };
+
+  let body = &arg_forms[1];
+
+  let lambda = expression::LambdaStruct
+  {
+    arg :Box::new(_args.clone()),
+    body:Box::new(body.clone())
+  };
+  Ok(expression::Expr::Lambda(lambda))
+}
+fn compute_lambda(lambda:&expression::LambdaStruct,
+                  args  :&[expression::Expr],
+                  env   :&mut env::Env) -> Result<expression::Expr, expression::Err> 
+{
+
+    //get the vector of expression
+    let fn_args = match &*lambda.arg
+    {
+      expression::Expr::List(val) => val,
+      _ =>
+      {
+          println!("can not extract lambda arguments!");
+          std::process::exit(-1);
+      }
+    };
+    if args.len() != fn_args.len()
+    {
+        println!("not enough arguments passed to lambda!");
+        std::process::exit(-1);
+    }
+
+    //create vector of arguments name
+    let names = {
+                  let mut names:Vec<String> = Vec::new();
+                  for arg in fn_args
+                  {
+                      match arg
+                      {
+                        expression::Expr::Symbol(val) => names.push(val.clone()),
+                        _ =>
+                        {
+                          println!("argument name must be string!");
+                          std::process::exit(-1);
+                        }
+                      }
+                  }
+                  names
+                };
+    
+    //init temporal environment
+    let mut temp_env = env::Env{data:HashMap::new()};
+
+    let max = names.len();
+    for counter in 0..max
+    {
+      temp_env.data.insert(names[counter].clone(),args[counter].clone());
+    }
+    temp_env = env::unite_environments(env,&temp_env);
+
+    let result = eval(&lambda.body,&mut temp_env);
+    *env = env::substract_environments(env,&mut temp_env);
+    match result
+    {
+      Ok(_) => result,
+      Err(val) =>
+      {
+        match val
+        {
+          expression::Err::Reason(v) =>
+          {
+            println!("{}",v);
+            std::process::exit(-1);
+          }
+        }
+      }
+    }
+
+}
+fn process_lambda(args  :&[expression::Expr],
+                  env   :&mut env::Env) -> Result<expression::Expr, expression::Err>
+{
+  if args.len() == 2
+  {
+    Ok(define_lambda(args).unwrap())
+  }
+  else if args.len() > 2
+  {
+    //define lambda and compute it
+    let lambda = match define_lambda(&args[0..2]).unwrap()
+                {
+                  expression::Expr::Lambda(val) => val,
+                  _ =>  {
+                           println!("gz! you reached the impossible point!");
+                           std::process::exit(-2);
+                        }
+                };
+
+    let result = compute_lambda(&lambda,&args[2..],env).unwrap();
+    Ok(result)
+  }
+  else
+  {
+    println!("can not process lambda!\n not enough arguments!");
+    std::process::exit(-1);
+  }
+}
+
 
 fn print_data(arg_forms: &[expression::Expr],env:&mut env::Env)
 {
